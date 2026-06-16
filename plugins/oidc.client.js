8f6117f3ea36932232655a83fc17021554950eb2
LucieFB <lucie.febvre-bonin@coda-student.school>
2026-06-16 16:06:22 +0200

    feat: variables d'environnement 3.0

--- plugins/oidc.client.js
+++ plugins/oidc.client.js
@@ -4,8 +4,8 @@ export default defineNuxtPlugin(() => {
   const manager = new UserManager({
     authority: import.meta.env.VITE_ZITADEL_ISSUER,
     client_id: import.meta.env.VITE_ZITADEL_CLIENT_ID,
-    redirect_uri: 'http://localhost:3000/auth/callback',
-    post_logout_redirect_uri: 'http://localhost:3000/',
+    redirect_uri: import.meta.env.VITE_ZITADEL_REDIRECT_URI,
+    post_logout_redirect_uri: import.meta.env.VITE_ZITADEL_POST_LOGOUT_REDIRECT_URI,
     response_type: 'code',
     scope: 'openid profile email',
     userStore: new WebStorageStateStore({ store: window.localStorage }),
