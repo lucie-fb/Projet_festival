e2ea8d4d3c814a3e42ff22d7ebd320aae38c8649
LucieFB <lucie.febvre-bonin@coda-student.school>
2026-06-16 16:14:00 +0200

    feat: variables d'environnement 3.0

--- tests/basic.spec.js
+++
@@ -1,7 +0,0 @@
-import { describe, it, expect } from 'vitest';
-
-describe('Vérification du fonctionnement de la suite de tests', () => {
-  it('devrait passer avec succès', () => {
-    expect(1 + 1).toBe(2);
-  });
-});
